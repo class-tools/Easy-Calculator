f057762b1d4a07e1d2e4c9bd07bef064ea10d434
Yuchen Ren <ldfx_ryc@163.com>
2022-03-24 19:03:15 +0800

    VER 1.0.0.1
    x- Fix some details.
    -- Add Chinese version readme.
    -- Add ".gitignore" file to ignore ".exe" files and other unnecessary files.

--- README.zh-Hans.md
+++ README.zh-Hans.md
@@ -1,18 +1,18 @@
-# Easy Calculator
+# 简易计算器
 
-An easy calculator made by C++.
+一个用 C++ 编写的简易的计算器。
 
-**English** | [简体中文](./README.zh-Hans.md)
+[English](./README.md) | **简体中文**
 
-### How to use
+### 使用方法
 
-命令:
+命令：
 
-`1 x y` means that the value of expression y is assigned to variable x.
+`1 x y` 表示表达式 y 的值被赋给变量 x。
 
-`2 x` indicates the value of the calculated expression x.
+`2 x` 表示计算表达式 x 的值。
 
-### Contributors
+### 贡献名单
 
 | <img src="https://avatars.githubusercontent.com/u/67366523?v=4" width="60px"></br> tigerchen-coder |
 | :---: |
--- easy-calculater.cpp
+++ easy-calculater.cpp
@@ -1,3 +1,9 @@
+/*
+Web-Class-Helper Main File 1.0.0
+This source code file is under MIT License.
+Copyright (c) 2022 Class Tools Develop Team
+Contributors: tigerchen-coder
+*/
 #include<iostream>
 #include<cstring>
 #include<string>
@@ -5,14 +11,15 @@
 #include<stack>
 #include<map>
 using namespace std;
+
 const int maxn = 100005;
-struct bign{//高精板子 
-    int d[maxn], len;
- 
+
+// Big Number Template
+struct bign{
+	int d[maxn], len;
 	void clean() { while(len > 1 && !d[len-1]) len--; }
- 
-    bign() 			{ memset(d, 0, sizeof(d)); len = 1; }
-    bign(int num) 	{ *this = num; } 
+	bign() 			{ memset(d, 0, sizeof(d)); len = 1; }
+	bign(int num) 	{ *this = num; }
 	bign(char* num) { *this = num; }
 	bign(string num){ *this = num; }
 	bign operator = (const string num){
@@ -22,107 +29,103 @@ struct bign{//高精板子
 		clean();
 		return *this;
 	}
-    bign operator = (const char* num){
-        memset(d, 0, sizeof(d)); len = strlen(num);
-        for(int i = 0; i < len; i++) d[i] = num[len-1-i] - '0';
-        clean();
+	bign operator = (const char* num){
+		memset(d, 0, sizeof(d)); len = strlen(num);
+		for(int i = 0; i < len; i++) d[i] = num[len-1-i] - '0';
+		clean();
 		return *this;
-    }
-    bign operator = (int num){
-        char s[20]; sprintf(s, "%d", num);
-        *this = s;
+	}
+	bign operator = (int num){
+		char s[20]; sprintf(s, "%d", num);
+		*this = s;
 		return *this;
-    }
- 
-    bign operator + (const bign& b){
-        bign c = *this; int i;
-        for (i = 0; i < b.len; i++){
-        	c.d[i] += b.d[i];
-        	if (c.d[i] > 9) c.d[i]%=10, c.d[i+1]++;
+	}
+	bign operator + (const bign& b){
+		bign c = *this; int i;
+		for (i = 0; i < b.len; i++){
+			c.d[i] += b.d[i];
+			if (c.d[i] > 9) c.d[i]%=10, c.d[i+1]++;
 		}
 		while (c.d[i] > 9) c.d[i++]%=10, c.d[i]++;
 		c.len = max(len, b.len);
 		if (c.d[i] && c.len <= i) c.len = i+1;
-        return c;
-    }
-    bign operator - (const bign& b){
-        bign c = *this; int i;
-        for (i = 0; i < b.len; i++){
-        	c.d[i] -= b.d[i];
-        	if (c.d[i] < 0) c.d[i]+=10, c.d[i+1]--;
+		return c;
+	}
+	bign operator - (const bign& b){
+		bign c = *this; int i;
+		for (i = 0; i < b.len; i++){
+			c.d[i] -= b.d[i];
+			if (c.d[i] < 0) c.d[i]+=10, c.d[i+1]--;
 		}
 		while (c.d[i] < 0) c.d[i++]+=10, c.d[i]--;
 		c.clean();
 		return c;
-    }
-    bign operator * (const bign& b)const{
-        int i, j; bign c; c.len = len + b.len; 
-        for(j = 0; j < b.len; j++) for(i = 0; i < len; i++) 
+	}
+	bign operator * (const bign& b)const{
+		int i, j; bign c; c.len = len + b.len; 
+		for(j = 0; j < b.len; j++) for(i = 0; i < len; i++) 
 			c.d[i+j] += d[i] * b.d[j];
-        for(i = 0; i < c.len-1; i++)
-            c.d[i+1] += c.d[i]/10, c.d[i] %= 10;
-        c.clean();
+		for(i = 0; i < c.len-1; i++)
+			c.d[i+1] += c.d[i]/10, c.d[i] %= 10;
+		c.clean();
 		return c;
-    }
-    bign operator / (const bign& b){
-    	int i, j;
+	}
+	bign operator / (const bign& b){
+		int i, j;
 		bign c = *this, a = 0;
-    	for (i = len - 1; i >= 0; i--)
-    	{
-    		a = a*10 + d[i];
-    		for (j = 0; j < 10; j++) if (a < b*(j+1)) break;
-    		c.d[i] = j;
-    		a = a - b*j;
-    	}
-    	c.clean();
-    	return c;
-    }
-    bign operator % (const bign& b){
-    	int i, j;
+		for (i = len - 1; i >= 0; i--)
+		{
+			a = a*10 + d[i];
+			for (j = 0; j < 10; j++) if (a < b*(j+1)) break;
+			c.d[i] = j;
+			a = a - b*j;
+		}
+		c.clean();
+		return c;
+	}
+	bign operator % (const bign& b){
+		int i, j;
 		bign a = 0;
-    	for (i = len - 1; i >= 0; i--)
-    	{
-    		a = a*10 + d[i];
-    		for (j = 0; j < 10; j++) if (a < b*(j+1)) break;
-    		a = a - b*j;
-    	}
-    	return a;
-    }
+		for (i = len - 1; i >= 0; i--)
+		{
+			a = a*10 + d[i];
+			for (j = 0; j < 10; j++) if (a < b*(j+1)) break;
+			a = a - b*j;
+		}
+		return a;
+	}
 	bign operator += (const bign& b){
-        *this = *this + b;
-        return *this;
-    }
- 
-    bool operator <(const bign& b) const{
-        if(len != b.len) return len < b.len;
-        for(int i = len-1; i >= 0; i--)
-            if(d[i] != b.d[i]) return d[i] < b.d[i];
-        return false;
-    }
-    bool operator >(const bign& b) const{return b < *this;}
-    bool operator<=(const bign& b) const{return !(b < *this);}
-    bool operator>=(const bign& b) const{return !(*this < b);}
-    bool operator!=(const bign& b) const{return b < *this || *this < b;}
-    bool operator==(const bign& b) const{return !(b < *this) && !(b > *this);}
- 
-    string str() const{
-        char s[maxn]={};
-        for(int i = 0; i < len; i++) s[len-1-i] = d[i]+'0';
-        return s;
-    }
+		*this = *this + b;
+		return *this;
+	}
+	bool operator <(const bign& b) const{
+		if(len != b.len) return len < b.len;
+		for(int i = len-1; i >= 0; i--)
+			if(d[i] != b.d[i]) return d[i] < b.d[i];
+		return false;
+	}
+	bool operator >(const bign& b) const{return b < *this;}
+	bool operator<=(const bign& b) const{return !(b < *this);}
+	bool operator>=(const bign& b) const{return !(*this < b);}
+	bool operator!=(const bign& b) const{return b < *this || *this < b;}
+	bool operator==(const bign& b) const{return !(b < *this) && !(b > *this);}
+	string str() const{
+		char s[maxn]={};
+		for(int i = 0; i < len; i++) s[len-1-i] = d[i]+'0';
+		return s;
+	}
 }jia,jian,cheng,chu,zkh;istream& operator >> (istream& in, bign& x){
-    string s;
-    in >> s;
-    x = s.c_str();
-    return in;
+	string s;
+	in >> s;
+	x = s.c_str();
+	return in;
 }ostream& operator << (ostream& out, const bign& x){
-    out << x.str();
-    return out;
+	out << x.str();
+	return out;
 }
 stack<bign> st;
 map<string,bign> mp;
-bign zuo(string s){//计算 
-	//cout<<"1"<<endl;
+bign zuo(string s){
 	while(!st.empty()) st.pop();
 	char c;
 	int l=s.size();
@@ -143,9 +146,7 @@ bign zuo(string s){//计算
 				now=now/st.top();
 				st.pop();
 			}
-			//cout<<"QA";
 			if(get) st.push(now);
-			//cout<<"Q"<<endl;
 			now=0;
 			get=0;
 		}
@@ -178,7 +179,6 @@ bign zuo(string s){//计算
 			now=0;
 			continue;
 		}
-		//cout<<i<<"ok"<<endl;
 		if(c>='0'&&c<='9') now=now*10+(c-'0'),get=1;
 	}
 	return mp["_"]=st.top();
@@ -187,15 +187,13 @@ int main(){
 	jia.len=-1;jian.len=-2;cheng.len=-3;chu.len=-4;zkh.len=-5;
 	ios::sync_with_stdio(0);
 	int op;string x,y;
-	//cout<<"???"<<endl;
-	//cout<<zuo("0")<<endl;
 	cout<<"Easy calculater VER 1.0.0"<<endl;
-    cout<<"Input \"1 X Y\" assign Y to X (X is a string, Y is an expression or a number)"<<endl;
-    cout<<"Input \"2 X\" evaluates the value of expression X"<<endl;
-    cout<<"\"_\" means last result,press ENTER to confirm"<<endl;
-    cout<<"Do not input anything that does not conform to the format"<<endl;
+	cout<<"Input \"1 X Y\" assign Y to X (X is a string, Y is an expression or a number)"<<endl;
+	cout<<"Input \"2 X\" evaluates the value of expression X"<<endl;
+	cout<<"\"_\" means last result,press ENTER to confirm"<<endl;
+	cout<<"Do not input anything that does not conform to the format"<<endl;
 	while(1){
-		cout<<">>>";
+		cout<<">>> ";
 		cin>>op;
 		if(op==1){
 			cin>>x>>y;
